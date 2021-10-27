c0240ff247a5d38b6f23653787b22bb8f719ae58
GBaldez <83733139+GBaldez@users.noreply.github.com>
2021-10-27 14:00:43 -0300

    funções para manipulação da tabela de usuarios

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -9,24 +9,30 @@ usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction)
     res.status(200).send(users);
 });
 
-usersRoute.get('/user/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.get('/user/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const user = req.params.uuid;
-    res.status(StatusCodes.OK).send({ user });
+    const foundUser = await userRepository.findById(user);
+    res.status(StatusCodes.OK).send({ foundUser });
 });
 
-usersRoute.post('/users',(req: Request, res: Response, next: NextFunction) => {
+usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction) => {
     const newUser = req.body;
-    res.status(StatusCodes.CREATED).send(newUser);
+    const user = await userRepository.create(newUser);
+    res.status(StatusCodes.CREATED).send(user);
 });
 
-usersRoute.put('/user/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.put('/user/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const user = req.params.uuid;
     const modifiedUser = req.body;
     modifiedUser.uuid = user;
-    res.status(StatusCodes.OK).send(modifiedUser);
+
+    await userRepository.update(modifiedUser);
+    res.status(StatusCodes.OK).send();
 });
 
-usersRoute.delete('/user/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.delete('/user/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    await userRepository.remove(uuid);
     res.status(StatusCodes.OK);
 });
 
